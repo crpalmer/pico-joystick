0e7aaad40552d685b9461380248e76725a3aac5c
Christopher R. Palmer <crpalmer@gmail.com>
2025-01-03 08:00:33 -0500

    test-gamepad: fix wakeup from deep sleep

--- test-gamepad.cpp
+++ test-gamepad.cpp
@@ -11,7 +11,7 @@ void configure_button(GPInput *button) {
 }
 
 static void threads_main(int argc, char **argv) {
-    pico_joystick_on_boot("test-gamepad", 28, 0, 27, 26);
+    pico_joystick_on_boot("test-gamepad", 28, 28, 27, 26);
     new Button(0, 28, "test-button");
     pico_joystick_start("test gamepad");
 }
