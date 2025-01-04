1bfa178aeb735542af9e3a1af15b21dee176fee8
Christopher R. Palmer <crpalmer@gmail.com>
2025-01-04 11:15:27 -0500

    joystick: name all the gpios
    This will make it easier for me to setup the new controller so
    that it matches the configuration in batocera.

--- joystick.cpp
+++ joystick.cpp
@@ -14,18 +14,18 @@ void configure_button(GPInput *button) {
 
 static void threads_main(int argc, char **argv) {
     pico_joystick_on_boot("pico-joystick", WAKE_GPIO, WAKE_GPIO, 21, 27, 9);
-    new Button(31, 2, "joy1");
-    new Button(30, 3, "joy2");
-    new Button(29, 4, "joy3");
-    new Button(28, 5, "joy4");
-    new Button(27, 6, "little1");
-    new Button(26, 14, "little2");
-    new Button(25, 17, "little3");
-    new Button(24, 9, "blue");
-    new Button(23, 28, "red");
+    new Button(31, 2, "down");
+    new Button(30, 3, "up");
+    new Button(29, 4, "right");
+    new Button(28, 5, "left");
+    new Button(27, 6, "meta");
+    new Button(26, 14, "start");
+    new Button(25, 17, "select");
+    new Button(24, 9, "button-2");
+    new Button(23, 28, "button-1");
+    new Button(22, 27, "button-3");
     pico_joystick_start("Pico Joystick");
 }
-    
     
 int main(int argc, char **argv) {
    pi_init_with_threads(threads_main, argc, argv);
