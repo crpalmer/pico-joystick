4be32a232ef0826f8706756ef3fc8ad2b574c015
Christopher R. Palmer <crpalmer@gmail.com>
2025-01-19 15:48:32 -0500

    add x/y axis

--- gamepad.h
+++ gamepad.h
@@ -68,10 +68,11 @@ public:
 	int byte = id/8;
 	int bit = id%8;
 
+	uint8_t old_state = state[byte];
 	if (value) state[byte] |= (1 << bit);
 	else state[byte] &= ~(1 << bit);
 
-	hid->request_can_send_now();
+	if (old_state != state[byte]) hid->request_can_send_now();
     }
 
 private:
@@ -79,6 +80,60 @@ private:
     uint8_t *state;
     int n_buttons;
     int button_range[2];
+};
+
+class HIDXY : public HIDPage {
+public:
+    HIDXY(HID *hid) : hid(hid) {
+   }
+
+
+   int add_descriptor(uint8_t *descriptor) override {
+	int i = 0;
+	descriptor[i++] = 0x05;
+	descriptor[i++] = 0x01;	// USAGE_PAGE (Generic Desktop Controls)
+	descriptor[i++] = 0x09;
+	descriptor[i++] = 0x30; // USAGE (X)
+	descriptor[i++] = 0x09;
+	descriptor[i++] = 0x31; // USAGE (Y)
+	descriptor[i++] = 0x15;
+	descriptor[i++] = -127;    // LOGICAL_MINIMUM
+	descriptor[i++] = 0x25;
+	descriptor[i++] = 127;  // LOGICAL_MAXIMUM
+	descriptor[i++] = 0x95;
+	descriptor[i++] = 2;    // REPORT_COUNT
+	descriptor[i++] = 0x75;
+	descriptor[i++] = 8;    // REPORT_SIZE
+	descriptor[i++] = 0x81;
+	descriptor[i++] = 0x02; // INPUT (Data,Var,Abs)
+
+	return i;
+    }
+
+    int get_report_size() override {
+	return 2;
+    }
+
+    void fill_report(uint8_t *buf) override {
+	buf[0] = x;
+	buf[1] = y;
+    }
+
+    void move(double x_pct, double y_pct) {
+	int8_t x = (x_pct * 254 - 127);
+	int8_t y = (y_pct * 254 - 127);
+
+	if (x != this->x || y != this->y) {
+	    this->x = x;
+	    this->y = y;
+	    hid->request_can_send_now();
+	} 
+    }
+
+private:
+    HID *hid;
+    int8_t x = 0;
+    int8_t y = 0;
 };
 
 class Gamepad : public HID {
@@ -93,11 +148,13 @@ public:
     void initialize(const char *name) {
 	int descriptor_len = 0;
 	descriptor[descriptor_len++] = 0x05;
-	descriptor[descriptor_len++] = 0x01;                    // USAGE_PAGE (Generic Desktop)
+	descriptor[descriptor_len++] = 0x01; // USAGE_PAGE (Generic Desktop)
 	descriptor[descriptor_len++] = 0x09;
-	descriptor[descriptor_len++] = 0x04;                    // USAGE (Gamepad)
+	descriptor[descriptor_len++] = 0x04; // USAGE (Gamepad)
 	descriptor[descriptor_len++] = 0xa1;
-	descriptor[descriptor_len++] = 0x01;                    // COLLECTION (Application)
+	descriptor[descriptor_len++] = 0x01; // COLLECTION (Application)
+	descriptor[descriptor_len++] = 0xa1;
+	descriptor[descriptor_len++] = 0x00; // COLLECTION (Physical)
 
 	report_size = 1;
 	for(auto page : hid_pages) {
@@ -105,6 +162,7 @@ public:
 	    report_size += page->get_report_size();
 	}
 
+	descriptor[descriptor_len++] = 0xc0;                    // END_COLLECTION
 	descriptor[descriptor_len++] = 0xc0;                    // END_COLLECTION
 
 	HID::initialize(name, descriptor, descriptor_len, subclass);
--- test-gamepad.cpp
+++ test-gamepad.cpp
@@ -1,9 +1,12 @@
 #include "pi.h"
+#include "pico-adc.h"
 #include "bluetooth/bluetooth.h"
 #include "gamepad.h"
 #include "pico-joystick.h"
 #include "pi-threads.h"
 #include "time-utils.h"
+
+#include "random-utils.h"
 
 Button *configure_test_button(Button *button) {
     button->set_pullup_up();
@@ -15,19 +18,24 @@ static void threads_main(int argc, char **argv) {
 
     Gamepad *gp = new Gamepad();
     HIDButtons *buttons = new HIDButtons(gp);
+    HIDXY *xy = new HIDXY(gp);
+
+    gp->add_hid_page(xy);
     gp->add_hid_page(buttons);
 
     configure_test_button(new Button(11, "test-button 1"))->set_button_id(buttons, 1);
     configure_test_button(new Button(12, "test-button 2"))->set_button_id(buttons, 2);
+    configure_test_button(new Button(10, "joystick button"))->set_button_id(buttons, 3);
 
     gp->initialize("Test Gamepad");
     bluetooth_start_gamepad("Test Gamepad");
 
+    ADC *adc = new PicoADC();
+
     while (1) {
-	ms_sleep(1000);
-	buttons->set_button(11, 1);
-	ms_sleep(10000);
-	buttons->set_button(11, 0);
+	double x = adc->read_percentage(0);
+	double y = adc->read_percentage(1);
+	xy->move(x, y);
     }
 }
     
