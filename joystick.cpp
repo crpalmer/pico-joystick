4942263e883e52801212798a75bb445b6f8fd6e1
Christopher R. Palmer <crpalmer@gmail.com>
2025-01-21 19:15:44 -0500

    joystick: Just poll the inputs as fast as we can

--- joystick.cpp
+++ joystick.cpp
@@ -28,73 +28,88 @@ public:
 	sleeper->prod();
 	Gamepad::on_connect();
 	led->on();
+	connected = true;
     }
 
     void on_disconnect() override {
 	led->off();
 	Gamepad::on_disconnect();
+	connected = false;
+    }
+
+    void wait_connected() {
+	while (! connected) ms_sleep(1000);
     }
 
 private:
     Output *led;
     DeepSleeper *sleeper;
+    bool connected = false;
 };
 
-static Button *configure_wakeup_game_button(Button *button) {
-    button->set_pullup_down();
-    button->set_is_inverted();
-    return button;
-}
+static struct {
+    int gpio;
+    bool pull_down;
+    const char *name;
+    GPInput *input;
+} buttons[] = {
+    { 2, false, "up" },
+    { 3, false, "down" },
+    { 4, false, "left" },
+    { 5, false, "right" },
+    {10, true,  "b1" },
+    {11, false, "b2" },
+    {12, false, "b3" },
+    { 6, false, "start" },
+    { 7, false, "select" },
+    { 8, false, "meta" },
+};
 
-static Button *configure_ui_button(Button *button) {
-    button->set_pullup_up();
-    button->set_debounce(10);
-    return button;
-}
+static const int n_buttons = sizeof(buttons) / sizeof(*buttons);
 
-static Button *configure_game_button(Button *button) {
-    button->set_pullup_up();
-    return button;
+static GPInput *get_button(const char *name) {
+    for (int i = 0; i < n_buttons; i++) {
+	if (strcmp(buttons[i].name, name) == 0) return buttons[i].input;
+    }
+    return NULL;
 }
 
 static void threads_main(int argc, char **argv) {
-    Button *start  = configure_ui_button(new Button(6, "start"));
-    Button *b1     = configure_wakeup_game_button(new Button(10, "button-1"));
+    for (int i = 0; i < n_buttons; i++) {
+	buttons[i].input = new GPInput(buttons[i].gpio);
+	if (buttons[i].pull_down) {
+	    buttons[i].input->set_pullup_down();
+	    buttons[i].input->set_is_inverted();
+	} else {
+	    buttons[i].input->set_pullup_up();
+	}
+    }
+
+    GPInput *start  = get_button("start");
+    GPInput *b1     = get_button("b1");
 
     pico_joystick_boot(b1, 10, start, "joystick");
 
     GPOutput *power_led = new GPOutput(19);
     power_led->on();
 
-    Gamepad *gp = new Joystick(new GPOutput(18), new Sleeper());
-    HIDButtons *buttons = new HIDButtons(gp, 1, 16);
-    gp->add_hid_page(buttons);
-    gp->initialize("Test Gamepad");
-
-    Button *up     = configure_game_button(new Button(2, "up"));
-    Button *down   = configure_game_button(new Button(3, "down"));
-    Button *left   = configure_game_button(new Button(4, "left"));
-    Button *right  = configure_game_button(new Button(5, "right"));
-    Button *select = configure_ui_button  (new Button(7, "select"));
-    Button *meta   = configure_ui_button  (new Button(8, "meta"));
-    Button *b2     = configure_game_button(new Button(11, "button-2"));
-    Button *b3     = configure_game_button(new Button(12, "button-3"));
-
-    up->set_button_id(buttons, 1);
-    down->set_button_id(buttons, 2);
-    left->set_button_id(buttons, 3);
-    right->set_button_id(buttons, 4);
-    b1->set_button_id(buttons, 5);
-    b2->set_button_id(buttons, 6);
-    b3->set_button_id(buttons, 7);
-    start->set_button_id(buttons, 8);
-    select->set_button_id(buttons, 9);
-    meta->set_button_id(buttons, 10);
-
+    Joystick *joystick = new Joystick(new GPOutput(18), new Sleeper());
+    HIDButtons *hid_buttons = new HIDButtons(joystick, 1, n_buttons+1);
+    joystick->add_hid_page(hid_buttons);
+    joystick->initialize("Test Gamepad");
     bluetooth_start_gamepad("Pico Joystick");
     new Sleeper();
+
+    while (1) {
+	joystick->wait_connected();
+	hid_buttons->begin_transaction();
+	for (int i = 0; i < n_buttons; i++) {
+	    hid_buttons->set_button(i+1, buttons[i].input->get());
+	}
+	hid_buttons->end_transaction();
+    }
 }
-    
+
 int main(int argc, char **argv) {
    pi_init_with_threads(threads_main, argc, argv);
 }
