c721dc48e106fb9c91a74bcd8c70cd1032ce56f8
Christopher R. Palmer <crpalmer@gmail.com>
2025-03-02 09:03:23 -0500

    thumbstick: add 4 different thumbstick mappings
    Switch the programming mode switch then press:
    
    8-way: b1
    4-way: b2
    q-bert: start
    prefer diagonals: select

--- thumbstick.cpp
+++ thumbstick.cpp
@@ -5,6 +5,73 @@
 #include "pico-adc.h"
 #include "pico-joystick.h"
 
+#define N_REGIONS 9
+
+#define CENTER 0
+#define UP    (1 << 0)
+#define DOWN  (1 << 1)
+#define LEFT  (1 << 2)
+#define RIGHT (1 << 3)
+#define SAME  (1 << 4)
+
+#define UL (UP | LEFT)
+#define UR (UP | RIGHT)
+#define LT (LEFT)
+#define RT (RIGHT)
+#define DL (DOWN | LEFT)
+#define DW (DOWN)
+#define DR (DOWN | RIGHT)
+#define SM (SAME)
+#define NO (CENTER)
+
+static uint8_t map_8_way[N_REGIONS * N_REGIONS] = {
+    UL, UL, UL, UP, UP, UP, UR, UR, UR,
+    UL, UL, UL, UP, UP, UP, UR, UR, UR,
+    UL, UL, UL, UP, UP, UP, UR, UR, UR,
+    LT, LT, LT, NO, NO, NO, RT, RT, RT,
+    LT, LT, LT, NO, NO, NO, RT, RT, RT,
+    LT, LT, LT, NO, NO, NO, RT, RT, RT,
+    DL, DL, DL, DW, DW, DW, DR, DR, DR,
+    DL, DL, DL, DW, DW, DW, DR, DR, DR,
+    DL, DL, DL, DW, DW, DW, DR, DR, DR,
+};
+
+static uint8_t map_4_way[N_REGIONS * N_REGIONS] = {
+    SM, UP, UP, UP, UP, UP, UP, UP, SM,
+    LT, SM, UP, UP, UP, UP, UP, SM, RT,
+    LT, LT, SM, UP, UP, UP, SM, RT, RT,
+    LT, LT, LT, NO, NO, NO, RT, RT, RT,
+    LT, LT, LT, NO, NO, NO, RT, RT, RT,
+    LT, LT, LT, NO, NO, NO, RT, RT, RT,
+    LT, LT, SM, DW, DW, DW, SM, RT, RT,
+    LT, SM, DW, DW, DW, DW, DW, SM, RT,
+    SM, DW, DW, DW, DW, DW, DW, DW, SM,
+};
+
+static uint8_t map_qbert[N_REGIONS * N_REGIONS] = {
+    LT, LT, LT, LT, SM, UP, UP, UP, UP,
+    LT, LT, LT, LT, SM, UP, UP, UP, UP,
+    LT, LT, LT, LT, NO, UP, UP, UP, UP,
+    LT, LT, LT, NO, NO, NO, UP, UP, UP,
+    SM, SM, NO, NO, NO, NO, NO, SM, SM,
+    DW, DW, DW, NO, NO, NO, RT, RT, RT,
+    DW, DW, DW, DW, NO, RT, RT, RT, RT,
+    DW, DW, DW, DW, SM, RT, RT, RT, RT,
+    DW, DW, DW, DW, SM, RT, RT, RT, RT,
+};
+
+static uint8_t map_prefer_diagonals[N_REGIONS * N_REGIONS] = {
+    UL, UL, SM, UP, UP, UP, SM, UR, UR,
+    UL, UL, UL, UP, UP, UP, UR, UR, UR,
+    SM, UL, UL, SM, UP, SM, UR, UR, SM,
+    LT, LT, SM, UL, NO, UR, SM, RT, RT,
+    LT, LT, LT, NO, NO, NO, RT, RT, RT,
+    LT, LT, SM, DL, NO, DR, SM, RT, RT,
+    SM, DL, DL, SM, DW, SM, DR, DR, SM,
+    DL, DL, DL, DW, DW, DW, DR, DR, DR,
+    DL, DL, SM, DW, DW, DW, SM, DR, DR,
+};
+
 class Sleeper : public DeepSleeper {
 public:
     Sleeper(int gpio) : DeepSleeper(gpio, 10*60*1000) {
@@ -50,17 +117,18 @@ static struct {
     const char *name;
     GPInput *input;
 } buttons[] = {
-    {-1, "up" },
+    {-1, "up" },	/* #1 */
     {-1, "down" },
     {-1, "left" },
     {-1, "right" },
-    {13, "b1" },
+    {13, "b1" },	/* #5 */
     { 9, "b2" },
     {-1, "b3" },
-    { 2, "start" },
-    { 5, "select" },
+    { 5, "start" },	/* #8 */
+    { 2, "select" },
     {-1, "meta" },
     {26, "thumb-switch" },
+    {21, "program-mode" },
 };
 
 static const int n_buttons = sizeof(buttons) / sizeof(*buttons);
@@ -72,9 +140,26 @@ static GPInput *get_button(const char *name) {
     return NULL;
 }
 
+static void dump_map(uint8_t *map) {
+    for (int y = 0; y < N_REGIONS; y++) {
+	if (y == 3 || y == 6) {
+	    for (int i = 0; i < N_REGIONS + 3; i++) printf("-");
+	    printf("\n");
+	}
+	for (int x = 0; x < N_REGIONS; x++) {
+	    uint8_t action = map[x + y*N_REGIONS];
+	    if (x == 3 || x == 6) printf("|");
+	    if (action == SAME) printf("|same");
+	    else printf("|%s%s%s%s", (action & LEFT) ? "L" : " ", (action & RIGHT) ? "R" : " ", (action & UP) ? "U" : " ", (action & DOWN) ? "D" : " ");
+	}
+	printf("|\n");
+    }
+}
+
 static void threads_main(int argc, char **argv) {
     for (int i = 0; i < n_buttons; i++) {
 	if (buttons[i].gpio >= 0) {
+	    printf("Initializing %s on %d\n", buttons[i].name, buttons[i].gpio);
 	    buttons[i].input = new GPInput(buttons[i].gpio);
 	    buttons[i].input->set_pullup_up();
 	}
@@ -83,7 +168,16 @@ static void threads_main(int argc, char **argv) {
     ADC *adc = new PicoADC();
 
     GPInput *start  = get_button("start");
+    GPInput *select = get_button("select");
     GPInput *b1     = get_button("b1");
+    GPInput *b2     = get_button("b2");
+    GPInput *program_mode = get_button("program-mode");
+
+    if (start == NULL) printf("FAILED TO GET START\n");
+    if (select == NULL) printf("FAILED TO GET SELECT\n");
+    if (b1 == NULL) printf("FAILED TO GET B1\n");
+    if (b2 == NULL) printf("FAILED TO GET B2\n");
+    if (program_mode == NULL) printf("FAILED TO GET PROGRAM-MODE\n");
 
     pico_joystick_boot(b1, 13, start, "joystick");
 
@@ -93,19 +187,47 @@ static void threads_main(int argc, char **argv) {
     joystick->initialize("Pico Thumbstick");
     bluetooth_start_gamepad("Pico Thumbstick");
 
+    uint8_t *map = map_8_way;
+
+    printf("Initial map:\n");
+    dump_map(map);
+
     while (1) {
 	joystick->wait_connected();
+
+	if (program_mode->get()) {
+	    uint8_t *new_map = map;
+
+	    if (b1->get()) new_map = map_8_way;
+	    else if (b2->get()) new_map = map_4_way;
+	    else if (start->get()) new_map = map_qbert;
+	    else if (select->get()) new_map = map_prefer_diagonals;
+
+	    if (new_map != map) {
+		map = new_map;
+		printf("Loaded map:\n");
+	        dump_map(map);
+	    }
+
+	    continue;
+	}
+
 	hid_buttons->begin_transaction();
 
 	double x = adc->read_percentage(2);
 	double y = adc->read_percentage(1);
 
-	for (int i = 1; i <= 4; i++) hid_buttons->set_button(i, false);
+	int x_region = (1 - x) * N_REGIONS;
+	int y_region = (1 - y) * N_REGIONS;
 
-	if (y > 0.75) hid_buttons->set_button(1, true);
-	if (y < 0.25) hid_buttons->set_button(2, true);
-	if (x > 0.75) hid_buttons->set_button(3, true);
-	if (x < 0.25) hid_buttons->set_button(4, true);
+	uint8_t action = map[x_region + y_region * N_REGIONS];
+
+	if (action != SAME) {
+	    hid_buttons->set_button(1, (action & UP) != 0);
+	    hid_buttons->set_button(2, (action & DOWN) != 0);
+	    hid_buttons->set_button(3, (action & LEFT) != 0);
+	    hid_buttons->set_button(4, (action & RIGHT) != 0);
+	}
 
 	for (int i = 0; i < n_buttons; i++) {
 	    if (buttons[i].gpio < 0) continue;
